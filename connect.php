729caf2d3ff3d6f53c890f3553f80ea725cf56a0
maissa70 <guezzimaissa@gmail.com>
2023-04-26 17:23:07 +0100

    new

---
+++ connect.php
@@ -0,0 +1,26 @@
+<?php
+
+// Récupération des données du formulaire
+//$FirstName = $_POST['FirstName'];
+//$LastName = $_POST['LastName'];
+//$email = $_POST['email'];
+//$password = $_POST['password'];
+
+// Connexion à la base de données MySQL
+//$conn = new mysqli('localhost', 'username', 'password', 'testing');
+//if ($conn->connect_error) {
+   // die('Connection failed: ' . $conn->connect_error);
+//}
+
+// Requête SQL pour insérer les données dans la table "users"
+//$sql = "INSERT INTO registration (FirstName, LastName, email, password ) VALUES (?, ?, ?, ?)";
+
+//if ($conn->query($sql) === TRUE) {
+  //  echo "Inscription réussie !";
+//} else {
+//    echo "Erreur: " . $sql . "<br>" . $conn->error;
+//}
+
+// Fermeture de la connexion à la base de données MySQL
+//$conn->close();
+//?>
